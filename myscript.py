ab4f263fccb7d60111de75abe9e54fb1b61e7b62
santhitak <63070175@kmitl.ac.th>
2023-03-02 10:01:33 +0700

    feat: sending parse csv from py as args

--- myscript.py
+++ myscript.py
@@ -1,12 +1,12 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("name", help="the name to greet")
-parser.add_argument("--age", help="the age of the person being greeted", type=int)
+parser.add_argument("memberList", help="the name list to greet", type=list)
 args = parser.parse_args()
 
-greeting = "Hello, " + args.name + "!"
-if args.age:
-    greeting += " You are " + str(args.age) + " years old."
+greeting = ""
+if args.memberList:
+    for item in memberList:
+        greeting += "Member No. " + item[0] + " " + item[1] + " " + item[2] + " is " + item[3] + " years old. \n"
 
 print(greeting)
--- parsingCSV.py
+++ parsingCSV.py
@@ -9,8 +9,5 @@ def extractCSV():
       for row in csvreader:
           rows.append(row)
 
-      for item in rows:
-        print("Member No. " + item[0] + " " + item[1] + " " + item[2] + " is " + item[3] + " years old")
-
 extractCSV()
 
