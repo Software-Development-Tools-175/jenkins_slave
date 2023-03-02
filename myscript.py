ddbdd5df2aceec8260575049c2c22811eb3e9f7d
santhitak <63070175@kmitl.ac.th>
2023-03-02 10:14:00 +0700

    check return value

--- myscript.py
+++ myscript.py
@@ -5,10 +5,11 @@ parser.add_argument("memberList", help="the name list to greet", type=string)
 args = parser.parse_args()
 
 convertStr = list(args.memberList)
+print(convertStr)
 
-greeting = ""
-if args.memberList:
-    for item in convertStr:
-        greeting += "Member No. " + item[0] + " " + item[1] + " " + item[2] + " is " + item[3] + " years old. \n"
+# greeting = ""
+# if args.memberList:
+#     for item in convertStr:
+#         greeting += "Member No. " + item[0] + " " + item[1] + " " + item[2] + " is " + item[3] + " years old. \n"
 
-print(greeting)
+# print(greeting)
